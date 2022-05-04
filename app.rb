bb23c7e7cc0b2482e67e32b6c1b6ca428bded8d9
timbo-graham <tmgraham29@gmail.com>
2022-05-04 14:26:04 +0100

    Added @name param and split -cat route

--- app.rb
+++ app.rb
@@ -17,7 +17,13 @@ get '/about-us' do
   "A page about sinatra"
 end
 
-get '/cat' do
+get '/random-cat' do
   @rand_name = ["Amigo", "Misty", "Almond"].sample
+  erb :index
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb :index
 end
